c17d552aee4a6661f49b2ca944d30fe7a887cef6
masonMO25 <masonneale@gmail.com>
2023-02-06 19:18:45 -0600

    fixed route order

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,6 +2,15 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.get('/', (req, res) => {
+    Comment.findAll({})
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
 router.post('/', withAuth, (req, res) => {
   if (req.session) {
     Comment.create({
@@ -15,15 +24,6 @@ router.post('/', withAuth, (req, res) => {
         res.status(400).json(err);
       });
   }
-});
-
-router.get('/', (req, res) => {
-    Comment.findAll({})
-      .then(dbCommentData => res.json(dbCommentData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
 });
 
 router.delete('/:id', withAuth, (req, res) => {
