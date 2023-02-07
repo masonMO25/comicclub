44e8fed3d84b2940b537710143a2efc58bc2ce67
masonMO25 <masonneale@gmail.com>
2023-02-06 18:16:06 -0600

    fixed errors with comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,22 +2,18 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
-  try {
-    const dbCommentData = await Comment.create({
-      user_id: req.body.user_id,
-      book_id: req.body.book_id,
+router.post('/', withAuth, (req, res) => {
+  if (req.session) {
+    Comment.create({
       comment_text: req.body.comment_text,
-    });
-
-    req.session.save(() => {
-      req.session.loggedIn = true;
-
-      res.status(200).json(dbCommentData);
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+      book_id: req.body.book_id,
+      user_id: req.session.user_id
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
   }
 });
 
@@ -28,21 +24,6 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
-});
-
-router.post('/', withAuth, (req, res) => {
-  if (req.session) {
-    Comment.create({
-      comment_text: req.body.comment_text,
-      book_id: req.body.book_id,
-      user_id: req.session.user_id,
-    })
-      .then(dbCommentData => res.json(dbCommentData))
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  }
 });
 
 router.delete('/:id', withAuth, (req, res) => {
