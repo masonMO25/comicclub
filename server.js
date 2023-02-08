e4bf6bb752ef9fe0d69d05e7d213f462b828d8b3
masonMO25 <masonneale@gmail.com>
2023-02-08 16:13:58 -0600

    fixed index

--- server.js
+++ server.js
@@ -25,14 +25,8 @@ app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 app.set('views', path.join(__dirname, "./views"));
-app.engine('handlebars', hbs.create({
-  handlebars: allowInsecurePrototypeAccess(hbs),
-  defaultLayout: 'main',
-  layoutsDir: app.get('views') + "/layouts",
-  partialsDir: [app.get('views') + '/partials'],
-}).engine);
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
